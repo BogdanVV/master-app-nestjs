004d417786ef66219a334fc9fde987289dd592ef
bogdanvv <bogdan.voinalovych@gmail.com>
2023-06-16 22:21:34 +0300

    feat: add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,7 +13,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  app.enableCors();
+  app.use(helmet());
   await app.listen(8888);
 }
 bootstrap();
